fe0c29b8d76650f24a812b7c5963280ba45c1291
shintsal <70944299+shintsal@users.noreply.github.com>
2021-01-21 17:59:55 +0200

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  serverId: number = 19;
+  serverStatus: string = 'offline';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  constructor() {
+    // Math.random() aropo luvun 0-1 --> molempien vaihtoehtojen tod.näk. 50%
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+   }
+
+  ngOnInit(): void {
+  }
+
+}
